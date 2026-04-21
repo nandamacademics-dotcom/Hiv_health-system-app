30524d4b129911ffcb95f5cdb379b6911dc919db
Maung Noah <maungnanda@Noahs-MacBook.local>
2026-04-21 17:03:46 +0700

    Update home page with welcome message and instructions

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="HIV System", layout="wide")
 
 st.title("HIV Health Information System")
-st.write("Use sidebar to navigate")
+st.write("Welcome to the HIV Health Information System")
+st.info("Select a page from the sidebar to get started")
